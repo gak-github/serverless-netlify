69a8a205d308259b40b71b912314c0d63c73b4c5
Asokumar Gurusamy <agurusamy@paypal.com>
2020-03-12 07:01:27 -0700

    removing environment specific endpoint

--- functions/serverless/utils/config.js
+++ functions/serverless/utils/config.js
@@ -4,8 +4,8 @@
 exports.BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8888' : process.env.BASE_URL
 
 exports.COOKIE_SECURE = process.env.NODE_ENV !== 'development'
-
-exports.ENDPOINT = process.env.NODE_ENV === 'development' ? '/.netlify/functions' : '/api'
+exports.ENDPOINT = '/.netlify/functions';
+// exports.ENDPOINT = process.env.NODE_ENV === 'development' ? '/.netlify/functions' : '/api'
 
 exports.GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
 exports.GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
