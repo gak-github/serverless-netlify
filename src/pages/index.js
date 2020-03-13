612fa817cc06e0e4a3909dfa9f375a4b4a38fbf6
Asokumar Gurusamy <agurusamy@paypal.com>
2020-03-12 19:37:50 -0700

    deleting header.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/App.css";
+import Header from "../components/Header";
 import Balance from "../components/Balance";
 import IncomeExpenses from "../components/IncomeExpenses";
 import TransactionList from "../components/TransactionList";
 import AddTransaction from "../components/AddTransaction";
 import { GlobalProvider } from "../context/GlobalState";
-import Header from "../components/Header";
 
 function IndexPage() {
   return (
