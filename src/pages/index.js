abadf4327fe840b3cf465150cc54ba90a40375ee
Asokumar Gurusamy <agurusamy@paypal.com>
2020-03-12 19:08:41 -0700

    fixing the build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../styles/App.css";
-import SEO from "../components/SEO";
-import Header from "../components/Header";
 import Balance from "../components/Balance";
 import IncomeExpenses from "../components/IncomeExpenses";
 import TransactionList from "../components/TransactionList";
 import AddTransaction from "../components/AddTransaction";
 import { GlobalProvider } from "../context/GlobalState";
+import SEO from "../components/SEO"
+import Header from "../components/Header"
 
 function IndexPage() {
   return (
